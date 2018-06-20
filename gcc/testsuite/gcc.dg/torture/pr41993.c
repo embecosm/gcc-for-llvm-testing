b0b18131f62049021989e756ff463cc83ce3b004
Edward Jones <ed.jones@embecosm.com>
2018-06-20 11:45:42 +0100

    AAP: Annotate tests with check for __builtin_return support
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/builtin-apply4.c: Annotate with check that
    	__builtin_return is supported.
    	* gcc.dg/builtin-return-1.c: Likewise.
    	* gcc.dg/pr55702.c: Likewise.
    	* gcc.dg/torture/pr41993.c: Likewise.
    	* gcc.dg/torture/stackalign/builtin-apply-4.c: Likewise.
    	* gcc.dg/torture/stackalign/builtin-return-1.c: Likewise.
    	* gcc.target/i386/20020218-1.c: Likewise.
    	* gcc.target/i386/avx-vzeroupper-27.c: Likewise.
    	* gcc.target/i386/builtin-apply-mmx.c: Likewise.
    	* lib/target-supports.exp (check_effective_target_builtin_return):
    	Added check to ensure that __builtin_return is supported.

--- gcc/testsuite/gcc.dg/torture/pr41993.c
+++ gcc/testsuite/gcc.dg/torture/pr41993.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-mavx -mvzeroupper" { target { i?86-*-* x86_64-*-* } } } */
 /* { dg-require-effective-target untyped_assembly } */
+/* { dg-require-effective-target builtin_return } */
 
 short retframe_short (void *rframe)
 {
