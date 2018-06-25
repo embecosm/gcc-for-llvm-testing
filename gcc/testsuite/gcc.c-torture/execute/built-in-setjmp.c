6e68e92db7f6b1ef48586fe96e2e1d0d32b7b3ba
Edward Jones <ed.jones@embecosm.com>
2018-06-25 14:24:31 +0100

    AAP: Annotate tests which use __builtin_setjmp or __builtin_longjmp
    Some targets do not support builtin versions of setjmp and longjmp.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.c-torture/execute/built-in-setjmp.c: Annotate test
    	to check for support for builtin versions of setjmp and
    	longjmp.
    	* gcc.c-torture/execute/pr60003.c: Likewise.
    	* gcc.dg/pr26983.c: Likewise.
    	* gcc.dg/setjmp-4.c: Likewise.
    	* gcc.dg/torture/pr79244.c: Likewise.
    	* lib/target-supports.exp (check_effective_target_builtin_setjmp_longjmp):
    	Add hook to check for support for builtin versions of setjmp and
    	longjmp.

--- gcc/testsuite/gcc.c-torture/execute/built-in-setjmp.c
+++ gcc/testsuite/gcc.c-torture/execute/built-in-setjmp.c
@@ -1,5 +1,6 @@
 /* { dg-require-effective-target indirect_jumps } */
 /* { dg-require-effective-target alloca } */
+/* { dg-require-effective-target builtin_setjmp_longjmp } */
 
 extern int strcmp(const char *, const char *);
 extern char *strcpy(char *, const char *);
