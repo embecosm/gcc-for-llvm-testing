d2e259b93fe4a096d6df417f482ecb3afa78c05b
Edward Jones <ed.jones@embecosm.com>
2018-06-27 11:58:10 +0100

    AAP: Add some missing dg annotations + minor fixes
    gcc/testsuite/ChangeLog:
    
    	* gcc.c-torture/execute/pr68381.c: Mark test with dg annotation
    	to check for support for __attribute__((noclone))
    	* gcc.dg/pr63342.c: Likewise.
    	* gcc.dg/torture/pr53272-2.c: Likewise.
    	* gcc.dg/pr68670-2.c: Fix spelling mistake in
    	dg-require-effective-target annotation.
    	* gcc.dg/torture/pr71020.c: Likewise.

--- gcc/testsuite/gcc.dg/pr68670-2.c
+++ gcc/testsuite/gcc.dg/pr68670-2.c
@@ -1,5 +1,6 @@
 /* PR rtl-optimization/68670 */
 /* { dg-do run } */
 /* { dg-options "-O2 -ftracer" } */
+/* { dg-require-effective-target noclone } */
 
 #include "../gcc.c-torture/execute/pr68376-2.c"
