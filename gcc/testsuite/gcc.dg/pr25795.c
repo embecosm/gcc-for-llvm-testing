07c3a8a134a295edf80da2d0772ba4300006b07b
Edward Jones <ed.jones@embecosm.com>
2018-06-27 12:29:45 +0100

    AAP: Add dg check that __attribute__((externally_visible)) is supported
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.c-torture/execute/bcp-1.c: Mark test with dg annotation
    	to check for support for __attribute__((externally_visible))
    	* gcc.dg/attr-externally-visible-1.c: Likewise.
    	* gcc.dg/attr-externally-visible-2.c: Likewise.
    	* gcc.dg/pr25795.c: Likewise.
    	* lib/target-supports.exp
    	(check_effective_target_externally_visible): Add a function to
    	check whether the target supports the externally_visible
    	attribute.

--- gcc/testsuite/gcc.dg/pr25795.c
+++ gcc/testsuite/gcc.dg/pr25795.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O3 -fwhole-program" } */
 /* { dg-final { scan-assembler "mystr" } } */
-
+/* { dg-require-effective-target externally_visible } */
 
 extern const char *mystr;       /* normally in a header */
 const char *mystr __attribute__ ((externally_visible));
