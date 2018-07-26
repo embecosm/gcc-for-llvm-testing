1ab3542bc634c093dffb486bb69144b5fd6a29f9
Edward Jones <ed.jones@embecosm.com>
2018-07-26 11:54:57 +0100

    Annotate gcc.dg/cpp/Wmissingdirs.c as requiring a gcc frontend
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/cpp/Wmissingdirs.c: Annotate as requiring a
    	gcc frontend, as the -Wmissing-include-dirs warning doesn't
    	emit anything with clang.

--- gcc/testsuite/gcc.dg/cpp/Wmissingdirs.c
+++ gcc/testsuite/gcc.dg/cpp/Wmissingdirs.c
@@ -1,5 +1,6 @@
 /* { dg-do preprocess } */
 /* { dg-options "-std=gnu99 -I /jolly/well/better/not/exist -Wmissing-include-dirs" } */
+/* { dg-require-effective-target gcc_frontend } */
 
 /* Test that -Wmissing-include-dirs issues a warning when a specified
    directory does not exist.  Source Ben Elliston, 2004-05-13.  */
