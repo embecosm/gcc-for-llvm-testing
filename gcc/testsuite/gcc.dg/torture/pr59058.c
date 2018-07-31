eca85536f050f3b6af728dd2c74b3631d75b4f1e
Edward Jones <ed.jones@embecosm.com>
2018-07-31 17:21:03 +0100

    Mark gcc.dg/torture/pr59058.c as needed gcc internals
    This test relies on undefined behavior which is exploited by
    LLVM when compiled with lto enabled. As a consequence the test
    fails when compiled with LTO.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/torture/pr59058.c: Mark as requiring gcc
    	internals.

--- gcc/testsuite/gcc.dg/torture/pr59058.c
+++ gcc/testsuite/gcc.dg/torture/pr59058.c
@@ -1,5 +1,11 @@
 /* { dg-do run } */
 
+/* This test iterates until 'b' wraps, however wrapping of signed
+   integers is undefined behavior. Based on this, and when compiled with -flto
+   it can be inferred that 'b' can never be set to 0, so the branch to 'l1' is
+   always taken.  */
+/* { dg-require-effective-target gcc_internals } */
+
 extern void abort (void);
 
 short b = 0;
