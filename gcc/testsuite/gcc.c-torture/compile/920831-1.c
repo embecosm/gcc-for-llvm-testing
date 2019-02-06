3a311b4bef5bad19b490b363cf7cbc8c6bbfedd8
lewis-revill <lewis.revill@embecosm.com>
2019-02-06 11:00:43 +0000

    Annotate tests which use an indirect goto to a non-label variable
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.c-torture/compile/920502-1.c: Mark test which uses an
    	indirect goto to a non-label variable.
    	* gcc.c-torture/compile/920826-1.c: Likewise.
    	* gcc.c-torture/compile/920831-1.c: Likewise.
    	* gcc.c-torture/compile/991213-3.c: Likewise.
    	* gcc.c-torture/compile/pr30984.c: Likewise.
    	* gcc.c-torture/compile/pr58164.c: Likewise.
    	* lib/target-supports.exp
    	(check_effective_target_nonlabel_goto): Add procedure to check
    	if target supports indirect goto to a non-label variable.

--- gcc/testsuite/gcc.c-torture/compile/920831-1.c
+++ gcc/testsuite/gcc.c-torture/compile/920831-1.c
@@ -1,3 +1,4 @@
 /* { dg-require-effective-target indirect_jumps } */
+/* { dg-require-effective-target nonlabel_goto } */
 
 f(x){goto*(char)x;}
--- gcc/testsuite/gcc.c-torture/compile/pr30984.c
+++ gcc/testsuite/gcc.c-torture/compile/pr30984.c
@@ -1,4 +1,5 @@
 /* { dg-require-effective-target indirect_jumps } */
+/* { dg-require-effective-target nonlabel_goto } */
 
 int fs_exec(int ino)
 {
