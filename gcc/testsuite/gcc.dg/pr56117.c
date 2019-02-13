308ad4f1fd2dd03a59535159b9f979322c2cb55a
lewis-revill <lewis.revill@embecosm.com>
2019-02-13 08:36:42 +0000

    Annotate tests which use -fsched2-use-superblocks
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.c-torture/compile/pr72749.c: Mark test which uses
    	-fsched2-use-superblocks as requiring gcc internals.
    	* gcc.dg/pr28726.c: Likewise.
    	* gcc.dg/pr41762.c: Likewise.
    	* gcc.dg/pr42396.c: Likewise.
    	* gcc.dg/pr49994-1.c: Likewise.
    	* gcc.dg/pr56117.c: Likewise.
    	* gcc.dg/pr83986.c: Likewise.

--- gcc/testsuite/gcc.dg/pr56117.c
+++ gcc/testsuite/gcc.dg/pr56117.c
@@ -2,6 +2,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -fsched2-use-superblocks" } */
 /* { dg-require-effective-target scheduling } */
+/* { dg-require-effective-target gcc_internals } */
 
 void
 foo (void *p)
