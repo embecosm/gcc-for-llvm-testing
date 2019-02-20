13d5fdf9cd52b12ecccce290271e0eef4ffcb689
lewis-revill <lewis.revill@embecosm.com>
2019-02-20 09:50:51 +0000

    Annotate tests with -Wno-unsequenced
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/builtins-10.c: Provide -Wno-unsequenced as an
    	additional option to prevent excess errors.
    	* gcc.dg/pr55010.c: Likewise.
    	* gcc.dg/torture/pr80842.c: Likewise.

--- gcc/testsuite/gcc.dg/pr55010.c
+++ gcc/testsuite/gcc.dg/pr55010.c
@@ -2,6 +2,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O2" } */
 /* { dg-additional-options "-march=i686" { target { { i?86-*-* x86_64-*-* } && ia32 } } } */
+/* { dg-additional-options "-Wno-unsequenced" } */
 
 long long int a;
 unsigned long long int b;
