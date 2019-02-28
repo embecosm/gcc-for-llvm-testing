1fba574ccaedf39f44ceef9cdbf4ff0e6b3a2dbe
Paolo Savini <paolo.savini@embecosm.com>
2019-02-28 12:37:18 +0000

    Annotate tests with -Wno-ignored-attributes
    gcc/testsuite/ChangeLog.Embecosm:
    
            * gcc.c-torture/compile/20011119-2.c: Provide
            -Wno-ignored-attributes as an additonal option to prevent
            excess errors.
            * gcc.dg/pr47276.c: Likewise.

--- gcc/testsuite/gcc.dg/pr47276.c
+++ gcc/testsuite/gcc.dg/pr47276.c
@@ -2,6 +2,7 @@
 /* { dg-require-alias "" } */
 /* { dg-require-visibility "" } */
 /* { dg-add-options bind_pic_locally } */
+/* { dg-additional-options "-Wno-ignored-attributes" } */
 
 #define ASMNAME(cname)  ASMNAME2 (__USER_LABEL_PREFIX__, cname)
 #define ASMNAME2(prefix, cname) STRING (prefix) cname
