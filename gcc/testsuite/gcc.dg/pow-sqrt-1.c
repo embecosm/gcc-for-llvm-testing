2527074f654f3c82784014dc2b1786ba67884003
lewis-revill <lewis.revill@embecosm.com>
2019-03-11 13:25:40 +0000

    Add -Wno-absolute-value as additional argument to tests
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/builtins-19.c: Add -Wno-absolute-value as an additional
    	option to prevent excess errors.
    	* gcc.dg/pow-sqrt-1.c: Likewise.
    	* gcc.dg/pow-sqrt-2.c: Likewise.
    	* gcc.dg/pow-sqrt-3.c: Likewise.
    	* gcc.dg/torture/builtin-math-6.c: Likewise.

--- gcc/testsuite/gcc.dg/pow-sqrt-1.c
+++ gcc/testsuite/gcc.dg/pow-sqrt-1.c
@@ -1,5 +1,6 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -ffast-math --param max-pow-sqrt-depth=5" } */
+/* { dg-additional-options "-Wno-absolute-value" } */
 
 #define EXPN (-6 * (0.5*0.5*0.5*0.5))
 
--- gcc/testsuite/gcc.dg/pow-sqrt-3.c
+++ gcc/testsuite/gcc.dg/pow-sqrt-3.c
@@ -1,5 +1,6 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -ffast-math --param max-pow-sqrt-depth=3" } */
+/* { dg-additional-options "-Wno-absolute-value" } */
 
 #define EXPN (1.25)
 #include "pow-sqrt.x"
