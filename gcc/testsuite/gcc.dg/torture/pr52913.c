eaf949c8fd1d6a2f26774e80c59f6e591bec0d62
lewis-revill <lewis.revill@embecosm.com>
2019-03-12 16:12:31 +0000

    Fix dangling-else warnings in tests
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/pr47899.c: Fix dangling else.
    	* gcc.dg/torture/pr52913.c: Likewise.

--- gcc/testsuite/gcc.dg/torture/pr52913.c
+++ gcc/testsuite/gcc.dg/torture/pr52913.c
@@ -8,12 +8,13 @@ lbl_101:
   e = 0;
 lbl_274:
   for (c = 0; c < 1; c = a)
-    if (d)
+    if (d) {
       if (b) {
 	goto lbl_101;
       } else {
 	break;
       }
+    }
   d = 1;
   goto lbl_274;
 }
