b7acac1722c21b743661728c22de286f42e5e6a6
lewis-revill <lewis.revill@embecosm.com>
2019-03-13 16:55:38 +0000

    Annotate tests using GNU extensions as requiring GCC internals
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/c11-align-8.c: Annotate test which uses GNU
    	extension(s) as requiring GCC internals.
    	* gcc.dg/c99-init-6.c: Likewise.
    	* gcc.dg/init-bad-4.c: Likewise.

--- gcc/testsuite/gcc.dg/init-bad-4.c
+++ gcc/testsuite/gcc.dg/init-bad-4.c
@@ -2,5 +2,6 @@
 /* Origin: Richard Guenther <rguenth@gcc.gnu.org> */
 /* { dg-do compile } */
 /* { dg-additional-options "-Wno-parentheses" } */
+/* { dg-require-effective-target gcc_internals } */
 
 struct A { } a = (struct A) {{ (X)0 }};  /* { dg-error "no members|extra brace|near|undeclared|constant|compound|excess" } */
