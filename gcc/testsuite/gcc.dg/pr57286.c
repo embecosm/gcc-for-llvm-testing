a1a1439f4448677977b2c2463d17b7e65d889e78
Paolo Savini <paolo.savini@embecosm.com>
2019-03-29 10:50:07 +0000

    Mark tests as requiring GCC frontend.
    gcc/testsuite/ChangeLog.Embecosm:
    
            * gcc.dg/pr57286.c: Mark as requiring GCC frontend.
            * gcc.dg/pr59261.c: Likewise.
            * gcc.dg/torture/pr69886.c: Likewise.

--- gcc/testsuite/gcc.dg/pr57286.c
+++ gcc/testsuite/gcc.dg/pr57286.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O" } */
+/* { dg-require-effective-target gcc_frontend } */
 
 typedef int vec __attribute__ ((vector_size (4*sizeof(int))));
 void f (vec *x){
