b0a4d9935c35e1e739236e172120f5a2b05303be
lewis-revill <lewis.revill@embecosm.com>
2019-04-25 13:31:14 +0100

    Mark -fmerge-all-constants check as requiring GCC frontend
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/20021029-2.c: Mark test for -fmerge-all-constants
    	behaviour as requiring GCC frontend.

--- gcc/testsuite/gcc.dg/20021029-2.c
+++ gcc/testsuite/gcc.dg/20021029-2.c
@@ -3,7 +3,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -fmerge-all-constants" } */
 /* { dg-final { scan-assembler-not ".rodata.cst" } } */
-/* { dg-additional-options "-Wno-ignored-optimization-argument" } */
+/* { dg-require-effective-target gcc_frontend } */
 
 int foo (int a)
 {
