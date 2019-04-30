b81aead7e82e7c03b969f558d03ec8b6fc815a0f
lewis-revill <lewis.revill@embecosm.com>
2019-04-30 15:00:52 +0100

    Annotate tests which require use of __builtin_complex
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/Wconversion-complex-c99.c: Annotate test which requires
    	use of __builtin_complex.
    	* gcc.dg/c11-complex-1.c: Likewise.
    	* gcc.dg/pr55019.c: Likewise.

--- gcc/testsuite/gcc.dg/pr55019.c
+++ gcc/testsuite/gcc.dg/pr55019.c
@@ -2,8 +2,7 @@
 /* { dg-options "-O1 -funroll-loops" } */
 /* { dg-add-options ieee } */
 /* { dg-require-effective-target builtin_isnan } */
-/* { dg-additional-options "-Wno-implicit-function-declaration" } */
-/* { dg-require-effective-target gcc_frontend } */
+/* { dg-require-effective-target builtin_complex } */
 
 extern void exit (int);
 extern void abort (void);
