4e96f3a3259426ab1222f8fa2ff6cac65cfc8655
lewis-revill <lewis.revill@embecosm.com>
2019-05-06 12:29:53 +0100

    Annotate test as requiring GCC frontend
    gcc/testsuite/ChangeLog.Embecosm:
    
    	* gcc.dg/pr28322.c: Annotate test for silently ignoring unknown
    	-Wno arguments as requiring GCC frontend.

--- gcc/testsuite/gcc.dg/pr28322.c
+++ gcc/testsuite/gcc.dg/pr28322.c
@@ -1,6 +1,7 @@
 /* PR28322: ignore unknown -Wno-* if no warning is emitted.  */
 /* { dg-do compile } */
 /* { dg-options " -Wno-foobar -Wno-div-by-zero" } */
+/* { dg-require-effective-target gcc_frontend } */
 
 void foo(void)
 {
