1040ae6782b5e443e3da27bcd133c8a5a85f177e
Scott Egerton <scott.egerton@embecosm.com>
2019-12-19 15:27:39 +0000

    Allow tests for UCNs to accept either UCNs or unicode equivalent
    GCC will emit everything as a UCN however Clang will emit unicode
    values.

--- gcc/testsuite/gcc.dg/cpp/ucnid-15.c
+++ gcc/testsuite/gcc.dg/cpp/ucnid-15.c
@@ -2,5 +2,5 @@
    -dD output with the original spelling.  */
 /* { dg-do preprocess } */
 /* { dg-options "-std=c99 -dD" } */
-/* { dg-final { scan-file ucnid-15.i "#define a\\(\\\\u00c1\\) x:\\\\u00C1:y:\\\\u00c1:z" } } */
 #define a(\u00c1) x:\u00C1:y:\u00c1:z
+/* { dg-final { scan-file ucnid-15.i "#define a\\((\\\\u00c1\\|Á))x:(\\\\u00C1:|Á)y:(\\\\u00c1|Á):z" } } */
